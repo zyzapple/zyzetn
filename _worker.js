e0d8e7afcb60f3ee64ba6fc4f92f493bc963c172
cmliu <cmliu@users.noreply.github.com>
2026-07-23 07:58:39 +0800

    fix: 移除_version_常量行首的多余字符

--- _worker.js
+++ _worker.js
@@ -1,4 +1,4 @@
-﻿const Version = '2026-07-22 19:14:26';
+const Version = '2026-07-22 19:14:26';
 let config_JSON, 缓存SOCKS5白名单 = null, 调试日志打印 = false;
 let SOCKS5白名单 = ['*tapecontent.net', '*cloudatacdn.com', '*loadshare.org', '*cdn-centaurus.com', 'scholar.google.com'];
 const Pages静态页面 = 'https://edt-pages.github.io';
